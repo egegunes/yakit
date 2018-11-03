be488d2b3905023442fd2c8b29165f106cdeee46
Ege Güneş <egegunes@gmail.com>
2018-11-03 22:46:05 +0300

    Add interface for Vehicle

--- yakit/vehicle.go
+++ yakit/vehicle.go
@@ -5,3 +5,11 @@ type Vehicle struct {
 	Year  int
 	Model Model
 }
+
+type VehicleService interface {
+	Vehicle(id string) (*Vehicle, error)
+	Vehicles(modelID string, brandID string) ([]Vehicle, error)
+	CreateVehicle(v Vehicle) (*Vehicle, error)
+	UpdateVehicle(v Vehicle) (*Vehicle, error)
+	DeleteVehicle(id string) error
+}
