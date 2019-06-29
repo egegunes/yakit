7c8b5695a57328599f9cd683fe1be72816e2612c
Ege Güneş <egegunes@gmail.com>
2019-06-29 16:05:03 +0300

    Add provision script

---
+++ scripts/provision.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DBCONTAINERNAME=postgres
+DBNAME=postgres
+DBUSER=postgres
+MIGRATIONSFOLDER=$(pwd)/db/migrations
+DUMMYDATAFOLDER=$(pwd)/db/dummy
+
+function run_migrations {
+    for migration in "$MIGRATIONSFOLDER"/*.sql; do
+        name=$(basename "$migration")
+        sudo docker cp "$migration" "$DBCONTAINERNAME":/"$name"
+        sudo docker exec "$DBCONTAINERNAME" psql -U "$DBUSER" -f "/$name" "$DBNAME"
+    done
+}
+
+
+function load_dummy_data {
+    for data in $DUMMYDATAFOLDER/*.sql; do
+        name=$(basename $data)
+        sudo docker cp "$data" "$DBCONTAINERNAME":/"$name"
+        sudo docker exec "$DBCONTAINERNAME" psql -U "$DBUSER" -f "/$name" "$DBNAME"
+    done
+}
+
+function main {
+    run_migrations
+    load_dummy_data
+}
+
+main
